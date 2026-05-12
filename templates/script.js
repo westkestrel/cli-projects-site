75ef08e9a996c19b9c69c7520e2e75a99ddfc34b
Mike West <westkestrel@gmail.com>
2026-05-12 11:11:24 -0700

    templates: move all klib .js and .css into a _lib folder
    The page now loads a single .js file named 'script.js', which includes all library .js files.
    It was already loading a single .css file named 'stylecss' which included all library .css files.

---
+++ templates/script.js
@@ -0,0 +1,6 @@
+{% include '_lib/filterbox.js' %}
+{% include '_lib/longpress.js' %}
+{% include '_lib/radio-checkbox-group.js' %}
+{% include '_lib/collapsible.js' %}
+{% include '_lib/stored-checkbox-state.js' %}
+{% include '_lib/navigation-without-bookmarks.js' %}
